ab992de60e30b07e1277bb19669926490cd6281f
Iran Reyes <iran.reyes@jam3.com>
2017-10-21 02:39:41 -0300

    ES5 code only

--- debug-popup-log.js
+++ debug-popup-log.js
@@ -1,53 +1,46 @@
+var toStyleString = require('to-style').string;
+var defaultValues = require('./defaultValues');
+
 function DebugPopupLog(documentContainer) {
   this._init(documentContainer);
 }
 
-DebugPopupLog.prototype._init = function(documentContainer) {
+DebugPopupLog.prototype._init = function(documentContainer, wrapperStyles) {
   this.container = document.createElement('div');
   this.container.id = 'j3-info-popup';
-  this.container.style.cssText = `
-              background-color: rgba(0,0,0,0.8);
-              min-width: 100px;
-              min-height: 45px;
-              position: fixed;
-              top: 10px;
-              right: 10px;
-              padding: 15px;
-              font-size: 16px;
-              color: white;
-              border-radius: 3px;
-              box-shadow: 0px 0px 7px 2px rgba(0,0,0,0.2);
-              line-height: 1.5;
-              font-family: sans-serif;
-          `;
 
-  const container = documentContainer || document.body;
+  var paramStyles = wrapperStyles || {};
+  var styles = Object.assign({}, defaultValues.defaultContainerStyles, paramStyles);
+
+  var stringifyStyles = toStyleString(defaultValues.defaultContainerStyles, { addUnits: true });
+
+  if (stringifyStyles) {
+    this.container.style.cssText = stringifyStyles;
+  }
+
+  var container = documentContainer || document.body;
   container.appendChild(this.container);
 };
 
-DebugPopupLog.prototype.addPanel = function(title = '', content = false) {
+DebugPopupLog.prototype.addPanel = function(title, content) {
   // Panel container
-  const panelElement = document.createElement('div');
+  var panelElement = document.createElement('div');
   panelElement.classList.add('panel');
 
   if (this.container.children.length > 0) {
-    panelElement.style.cssText = `
-                border-top: 1px solid #4a4444;
-                margin-top: 5px;
-                padding-top: 5px;
-            `;
+    panelElement.style.cssText = toStyleString(defaultValues.defaultPanelStyles);
   }
 
   if (title) {
-    const titleElement = document.createElement('div');
-    titleElement.style.cssText = 'color: #b1b0b0;';
+    var titleElement = document.createElement('div');
+    titleElement.style.cssText = toStyleString(defaultValues.defaultPanelTitleStyles);
     titleElement.innerText = title;
     panelElement.appendChild(titleElement);
   }
 
   if (content) {
-    const contentElement = document.createElement('div');
-    contentElement.style.cssText = 'font-size: 12px;';
+    var contentElement = document.createElement('div');
+    contentElement.style.cssText = toStyleString(defaultValues.defaultPanelContentStyles);
 
     // Refactor checking and use check-types module
     if (typeof content === 'number' || typeof content === 'string' || typeof content === 'boolean') {
@@ -55,8 +48,8 @@ DebugPopupLog.prototype.addPanel = function(title = '', content = false) {
       panelElement.appendChild(contentElement);
     } else if (typeof content === 'function') {
       panelElement.appendChild(contentElement);
-      const loop = function() {
-        const result = content();
+      var loop = function() {
+        var result = content();
 
         // Require the function returns a value
         if (result) {
---
+++ defaultValues.js
@@ -0,0 +1,27 @@
+module.exports = {
+  defaultContainerStyles: {
+    backgroundColor: 'rgba(0,0,0,0.8)',
+    minWidth: 100,
+    minHeight: 45,
+    position: 'fixed',
+    top: 10,
+    right: 10,
+    padding: 15,
+    fontSize: 16,
+    color: 'white',
+    borderRadius: 3,
+    boxShadow: '0px 0px 7px 2px rgba(0,0,0,0.2)',
+    fontFamily: 'sans-serif'
+  },
+  defaultPanelStyles: {
+    borderTop: '1px solid #4a4444',
+    marginTop: 5,
+    paddingTop: 5
+  },
+  defaultPanelTitleStyles: {
+    color: '#b1b0b0'
+  },
+  defaultPanelContentStyles: {
+    fontSize: 12
+  }
+};
